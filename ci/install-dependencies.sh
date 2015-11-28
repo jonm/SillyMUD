e124442b09a5cc463df536d8211570a6ff7d288d
Jon Moore <jonm@jjmoore.net>
2015-11-28 13:13:00 -0500

    Actually build and install Criterion.

--- ci/install-dependencies.sh
+++ ci/install-dependencies.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env sh
 
-git clone https://github.com/Snaipe/Criterion.git
+git clone https://github.com/Snaipe/Criterion.git && \
+cd Criterion && \
+git fetch origin master:master && \
+git checkout master && \
+mkdir build && \
+cd build && \
+cmake .. && \
+cmake --build . && \
+sudo make install
+
