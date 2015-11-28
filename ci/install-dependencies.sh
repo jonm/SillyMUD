6df8bec40a4ff7196567f41f26172c0e1e5ba8df
Jon Moore <jonm@jjmoore.net>
2015-11-28 13:30:49 -0500

    Create copyright file needed for Criterion CMake to complete.

--- ci/install-dependencies.sh
+++ ci/install-dependencies.sh
@@ -4,6 +4,7 @@ git clone https://github.com/Snaipe/Criterion.git && \
 cd Criterion && \
 git fetch origin master:master && \
 git checkout master && \
+cp LICENSE debian.copyright && \
 mkdir build && \
 cd build && \
 cmake .. && \
