5b0afbcb3daf5d3930d9ea8c8deb40a2943f294d
Dan Girellini <dan@longhands.org>
2015-12-10 14:10:57 -0800

    Add depecreated vers of add_command and cmd nodes
    The depcreated versions still use the command number. This gives a path
    to upgrade commands to stop using command numbers one at a time.

--- src/parser.h
+++ src/parser.h
@@ -13,11 +13,13 @@
 
 
 typedef struct command_node NODE;
-typedef void (*cmd_handler) (struct char_data * ch, char *arg, int cmd);
+typedef void (*cmd_handler) (struct char_data * ch, char *arg, const char *cmd);
+typedef void (*cmd_handler_dep) (struct char_data * ch, char *arg, int cmd);
 
 struct command_node {
   char name[64];
   cmd_handler func;
+  cmd_handler_dep func_dep;
   int number;
   byte min_pos;
   byte min_level;
@@ -32,7 +34,8 @@ struct radix_list {
   byte max_len;
 };
 
-void add_command(char *name, cmd_handler func, int number, int min_pos,
-                 int min_lev);
+void add_command(char *name, cmd_handler func, int min_pos, int min_lev);
+void add_command_dep(char *name, cmd_handler_dep func, int number, int min_pos,
+                     int min_lev);
 
 #endif
