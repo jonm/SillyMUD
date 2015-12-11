863eff53fbf4a778c22564b2dcc56a7cc1efbafb
Dan Girellini <dan@longhands.org>
2015-12-10 22:12:20 -0800

    Fix special moves

--- src/act.move.c
+++ src/act.move.c
@@ -143,9 +143,7 @@ int raw_move(struct char_data *ch, int dir) {
   bool has_boat;
   struct room_data *from_here, *to_here;
 
-  int special(struct char_data *ch, int dir, char *arg);
-
-  if (special(ch, dir + 1, "")) /* Check for special routines(North is 1) */
+  if (special(ch, move_dir_string[dir], "")) /* Check for special routines(North is 1) */
     return (FALSE);
 
   if (!valid_move(ch, dir)) {
